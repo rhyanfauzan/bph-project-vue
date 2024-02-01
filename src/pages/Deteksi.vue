2f3e57f04ac7d0d174757f2bf26d61ce5fe9c8a7
Ryan <rhyananiki@gmail.com>
2024-02-01 19:02:12 +0700

    Revisi Kolo

--- src/pages/Deteksi.vue
+++ src/pages/Deteksi.vue
@@ -4,20 +4,40 @@
   <div v-if="isFormBiodata" class="flex flex-col justify-center">
     <h5 class="mb-5 text-center text-xl font-bold">Biodata Pasien</h5>
     <div class="w-1/2 mt-3 mx-auto">
-      <Input v-model="nama" label="Nama" type="text" placeholder="Masukan nama.." size="md" />
+      <Input
+        v-model="nama"
+        label="Nama"
+        type="text"
+        placeholder="Masukan nama.."
+        size="md"
+      />
     </div>
     <div class="w-1/2 mt-3 mx-auto">
-      <Input v-model="usia" label="Usia" type="number" placeholder="Masukan usia.." size="md" />
+      <Input
+        v-model="usia"
+        label="Usia"
+        type="number"
+        placeholder="Masukan usia.."
+        size="md"
+      />
     </div>
     <div class="w-1/2 mt-3 mx-auto">
-      <Input v-model="alamat" label="Alamat" type="text" placeholder="Masukan alamat.." size="md" />
+      <Input
+        v-model="alamat"
+        label="Alamat"
+        type="text"
+        placeholder="Masukan alamat.."
+        size="md"
+      />
     </div>
     <Button class="w-1/2 mt-4 mx-auto" @click="nextStep"> Selanjutnya </Button>
   </div>
   <!-- isFormBiodata false tampilkan form deteksi  -->
   <div v-else class="flex flex-col justify-center">
     <div class="mt-3 mx-auto">
-      <h5 class="mb-5 text-center text-xl font-bold">Pilih Gejala Apa Yang Anda Rasakan</h5>
+      <h5 class="mb-5 text-center text-xl font-bold">
+        Pilih Gejala Apa Yang Anda Rasakan
+      </h5>
     </div>
     <!-- perbaris  1 -->
     <div class="w-1/2 flex mt-3 mx-auto">
@@ -25,10 +45,20 @@
         <p>Sering sakit pinggang?</p>
         <div class="flex">
           <div class="flex mr-2 mt-2 items-center">
-            <Radio v-model="sakitPinggang" label="Yes" name="radio-sakitPinggang" value="1" />
+            <Radio
+              v-model="sakitPinggang"
+              label="Yes"
+              name="radio-sakitPinggang"
+              value="1"
+            />
           </div>
           <div class="flex mt-2 items-center">
-            <Radio v-model="sakitPinggang" label="No" name="radio-sakitPinggang" value="0" />
+            <Radio
+              v-model="sakitPinggang"
+              label="No"
+              name="radio-sakitPinggang"
+              value="0"
+            />
           </div>
         </div>
       </div>
@@ -36,10 +66,20 @@
         <p>Kencing di malam hari lebih sering?</p>
         <div class="flex">
           <div class="flex mr-2 mt-2 items-center">
-            <Radio v-model="kencingMalam" label="Yes" name="radio-kencingMalam" value="1" />
+            <Radio
+              v-model="kencingMalam"
+              label="Yes"
+              name="radio-kencingMalam"
+              value="1"
+            />
           </div>
           <div class="flex mt-2 items-center">
-            <Radio v-model="kencingMalam" label="No" name="radio-kencingMalam" value="0" />
+            <Radio
+              v-model="kencingMalam"
+              label="No"
+              name="radio-kencingMalam"
+              value="0"
+            />
           </div>
         </div>
       </div>
@@ -52,10 +92,20 @@
         <p>Perlu mengejan saat awal kencing?</p>
         <div class="flex">
           <div class="flex mr-2 mt-2 items-center">
-            <Radio v-model="mengejan" label="Yes" name="radio-mengejan" value="1" />
+            <Radio
+              v-model="mengejan"
+              label="Yes"
+              name="radio-mengejan"
+              value="1"
+            />
           </div>
           <div class="flex mt-2 items-center">
-            <Radio v-model="mengejan" label="No" name="radio-mengejan" value="0" />
+            <Radio
+              v-model="mengejan"
+              label="No"
+              name="radio-mengejan"
+              value="0"
+            />
           </div>
         </div>
       </div>
@@ -63,10 +113,20 @@
         <p>Inkontensia urin/beser/susah menahan ketika ingin kencing?</p>
         <div class="flex">
           <div class="flex mr-2 mt-2 items-center">
-            <Radio v-model="inkontensia" label="Yes" name="radio-inkontensia" value="1" />
+            <Radio
+              v-model="inkontensia"
+              label="Yes"
+              name="radio-inkontensia"
+              value="1"
+            />
           </div>
           <div class="flex mt-2 items-center">
-            <Radio v-model="inkontensia" label="No" name="radio-inkontensia" value="0" />
+            <Radio
+              v-model="inkontensia"
+              label="No"
+              name="radio-inkontensia"
+              value="0"
+            />
           </div>
         </div>
       </div>
@@ -79,10 +139,20 @@
         <p>Urin lemah saat kencing?</p>
         <div class="flex">
           <div class="flex mr-2 mt-2 items-center">
-            <Radio v-model="urinLemah" label="Yes" name="radio-urinLemah" value="1" />
+            <Radio
+              v-model="urinLemah"
+              label="Yes"
+              name="radio-urinLemah"
+              value="1"
+            />
           </div>
           <div class="flex mt-2 items-center">
-            <Radio v-model="urinLemah" label="No" name="radio-urinLemah" value="0" />
+            <Radio
+              v-model="urinLemah"
+              label="No"
+              name="radio-urinLemah"
+              value="0"
+            />
           </div>
         </div>
       </div>
@@ -90,10 +160,20 @@
         <p>Nyeri saat kencing?</p>
         <div class="flex">
           <div class="flex mr-2 mt-2 items-center">
-            <Radio v-model="nyeriKencing" label="Yes" name="radio-nyeriKencing" value="1" />
+            <Radio
+              v-model="nyeriKencing"
+              label="Yes"
+              name="radio-nyeriKencing"
+              value="1"
+            />
           </div>
           <div class="flex mt-2 items-center">
-            <Radio v-model="nyeriKencing" label="No" name="radio-nyeriKencing" value="0" />
+            <Radio
+              v-model="nyeriKencing"
+              label="No"
+              name="radio-nyeriKencing"
+              value="0"
+            />
           </div>
         </div>
       </div>
@@ -106,10 +186,20 @@
         <p>Urin menetes setelah kencing?</p>
         <div class="flex">
           <div class="flex mr-2 mt-2 items-center">
-            <Radio v-model="urinMenetes" label="Yes" name="radio-urinMenetes" value="1" />
+            <Radio
+              v-model="urinMenetes"
+              label="Yes"
+              name="radio-urinMenetes"
+              value="1"
+            />
           </div>
           <div class="flex mt-2 items-center">
-            <Radio v-model="urinMenetes" label="No" name="radio-urinMenetes" value="0" />
+            <Radio
+              v-model="urinMenetes"
+              label="No"
+              name="radio-urinMenetes"
+              value="0"
+            />
           </div>
         </div>
       </div>
@@ -117,10 +207,20 @@
         <p>Terdapat darah ketika kencing?</p>
         <div class="flex">
           <div class="flex mr-2 mt-2 items-center">
-            <Radio v-model="darahUrin" label="Yes" name="radio-darahUrin" value="1" />
+            <Radio
+              v-model="darahUrin"
+              label="Yes"
+              name="radio-darahUrin"
+              value="1"
+            />
           </div>
           <div class="flex mt-2 items-center">
-            <Radio v-model="darahUrin" label="No" name="radio-darahUrin" value="0" />
+            <Radio
+              v-model="darahUrin"
+              label="No"
+              name="radio-darahUrin"
+              value="0"
+            />
           </div>
         </div>
       </div>
@@ -133,10 +233,20 @@
         <p>Kencing terasa tidak tuntas?</p>
         <div class="flex">
           <div class="flex mr-2 mt-2 items-center">
-            <Radio v-model="kencingTidakTuntas" label="Yes" name="radio-kencingTidakTuntas" value="1" />
+            <Radio
+              v-model="kencingTidakTuntas"
+              label="Yes"
+              name="radio-kencingTidakTuntas"
+              value="1"
+            />
           </div>
           <div class="flex mt-2 items-center">
-            <Radio v-model="kencingTidakTuntas" label="No" name="radio-kencingTidakTuntas" value="0" />
+            <Radio
+              v-model="kencingTidakTuntas"
+              label="No"
+              name="radio-kencingTidakTuntas"
+              value="0"
+            />
           </div>
         </div>
       </div>
@@ -144,20 +254,28 @@
         <p>Urin tidak keluar sama sekali?</p>
         <div class="flex">
           <div class="flex mr-2 mt-2 items-center">
-            <Radio v-model="urinTidakKeluar" label="Yes" name="radio-urinTidakKeluar" value="1" />
+            <Radio
+              v-model="urinTidakKeluar"
+              label="Yes"
+              name="radio-urinTidakKeluar"
+              value="1"
+            />
           </div>
           <div class="flex mt-2 items-center">
-            <Radio v-model="urinTidakKeluar" label="No" name="radio-urinTidakKeluar" value="0" />
+            <Radio
+              v-model="urinTidakKeluar"
+              label="No"
+              name="radio-urinTidakKeluar"
+              value="0"
+            />
           </div>
         </div>
       </div>
     </div>
     <!-- akhir perbaris  -->
-    <div class="space-20">
-    </div>
+    <div class="space-20"></div>
     <Button class="w-1/2 mt-4 mb-4 mx-auto" @click="submit"> Submit </Button>
-    <div class="space-80">
-    </div>
+    <div class="space-80"></div>
   </div>
 </template>
 
@@ -172,25 +290,25 @@
 </style>
 
 <script setup>
-import Navbar from "../layouts/Navbar.vue";
-import { ref } from "vue";
-import { Input, Button, Radio } from "flowbite-vue";
-import router from "../router";
+import Navbar from '../layouts/Navbar.vue';
+import { ref } from 'vue';
+import { Input, Button, Radio } from 'flowbite-vue';
+import router from '../router';
 
 const isFormBiodata = ref(true);
-const nama = ref("");
-const usia = ref("");
-const alamat = ref("");
-const sakitPinggang = ref("0");
-const kencingMalam = ref("0");
-const mengejan = ref("0");
-const inkontensia = ref("0");
-const urinLemah = ref("0");
-const nyeriKencing = ref("0");
-const urinMenetes = ref("0");
-const darahUrin = ref("0");
-const kencingTidakTuntas = ref("0");
-const urinTidakKeluar = ref("0");
+const nama = ref('');
+const usia = ref('');
+const alamat = ref('');
+const sakitPinggang = ref('0');
+const kencingMalam = ref('0');
+const mengejan = ref('0');
+const inkontensia = ref('0');
+const urinLemah = ref('0');
+const nyeriKencing = ref('0');
+const urinMenetes = ref('0');
+const darahUrin = ref('0');
+const kencingTidakTuntas = ref('0');
+const urinTidakKeluar = ref('0');
 
 // function nextstep
 function nextStep() {
@@ -201,41 +319,55 @@ function nextStep() {
 function submit() {
   isFormBiodata.value = true;
 
-  sessionStorage.setItem("nama", nama.value ?? "");
-  sessionStorage.setItem("usia", usia.value ?? 0);
-  sessionStorage.setItem("alamat", alamat.value ?? "");
+  sessionStorage.setItem('nama', nama.value ?? '');
+  sessionStorage.setItem('usia', usia.value ?? 0);
+  sessionStorage.setItem('alamat', alamat.value ?? '');
 
   fetchKNN();
 }
 
-
 function fetchKNN() {
   const formdata = new FormData();
-  formdata.append("umur", usia.value);
-  formdata.append("sakit_pinggang", sakitPinggang.value);
-  formdata.append("mengejan", mengejan.value);
-  formdata.append("urin_lemah", urinLemah.value);
-  formdata.append("urin_menetes", urinMenetes.value);
-  formdata.append("kencing_tidak_tuntas", kencingTidakTuntas.value);
-  formdata.append("nokturia", kencingMalam.value);
-  formdata.append("inkonsia_urin", inkontensia.value);
-  formdata.append("nyeri_saat_kencing", nyeriKencing.value);
-  formdata.append("darah_dalam_urin", darahUrin.value);
-  formdata.append("urin_tidak_keluar_sama_sekali", urinTidakKeluar.value);
+  formdata.append('umur', usia.value);
+  formdata.append('sakit_pinggang', sakitPinggang.value);
+  formdata.append('mengejan', mengejan.value);
+  formdata.append('urin_lemah', urinLemah.value);
+  formdata.append('urin_menetes', urinMenetes.value);
+  formdata.append('kencing_tidak_tuntas', kencingTidakTuntas.value);
+  formdata.append('nokturia', kencingMalam.value);
+  formdata.append('inkonsia_urin', inkontensia.value);
+  formdata.append('nyeri_saat_kencing', nyeriKencing.value);
+  formdata.append('darah_dalam_urin', darahUrin.value);
+  formdata.append('urin_tidak_keluar_sama_sekali', urinTidakKeluar.value);
+
+  // set to sessionStorage
+  sessionStorage.setItem('umur', usia.value);
+  sessionStorage.setItem('sakit_pinggang', sakitPinggang.value);
+  sessionStorage.setItem('mengejan', mengejan.value);
+  sessionStorage.setItem('urin_lemah', urinLemah.value);
+  sessionStorage.setItem('urin_menetes', urinMenetes.value);
+  sessionStorage.setItem('kencing_tidak_tuntas', kencingTidakTuntas.value);
+  sessionStorage.setItem('nokturia', kencingMalam.value);
+  sessionStorage.setItem('inkonsia_urin', inkontensia.value);
+  sessionStorage.setItem('nyeri_saat_kencing', nyeriKencing.value);
+  sessionStorage.setItem('darah_dalam_urin', darahUrin.value);
+  sessionStorage.setItem(
+    'urin_tidak_keluar_sama_sekali',
+    urinTidakKeluar.value
+  );
 
   var requestOptions = {
     method: 'POST',
     body: formdata,
   };
 
-  fetch("http://127.0.0.1:5000/api/v1/predict", requestOptions)
-    .then(response => response.json())
-    .then(result => {
+  fetch('http://127.0.0.1:5000/api/v1/predict', requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
       const hasil = result.response.status;
-      sessionStorage.setItem("hasil", hasil);
-      router.push("/hasil");
+      sessionStorage.setItem('hasil', hasil);
+      router.push('/hasil');
     })
-    .catch(error => console.log('error', error));
+    .catch((error) => console.log('error', error));
 }
-
 </script>
--- src/pages/Hasil.vue
+++ src/pages/Hasil.vue
@@ -2,37 +2,95 @@
   <Navbar />
   <div class="flex flex-col justify-center">
     <div class="flex justify-center items-center">
-      <TheCard class="w-full">
+      <TheCard class="w-full mt-4" style="max-width: 800px">
         <h5 class="text-xl my-4 font-bold text-center">Hasil Deteksi</h5>
-        <table class="text-sm">
+        <table class="text-md font-medium">
           <tr>
             <td>Nama</td>
-            <td id="nama_pasien"> : -</td>
+            <td id="nama_pasien">: -</td>
           </tr>
           <tr>
             <td>Usia</td>
-            <td id="usia_pasien"> : -</td>
+            <td id="usia_pasien">: -</td>
           </tr>
           <tr>
             <td>Alamat</td>
-            <td id="alamat_pasien"> : -</td>
+            <td id="alamat_pasien">: -</td>
           </tr>
         </table>
-        <div class=" mt-3 mx-auto">
-
-          <p class="mt-4 text-center">Anda terindikasi gejala</p>
-          <div class="showHasil mt-4 rounded text-white w-1/2 mx-auto text-center"
-            :class="{ 'bg-red-500': isRedBackground, 'bg-yellow-400': isYellowBackground, 'bg-green-500': isGreenBackground }">
+        <hr class="mt-3" />
+        <!-- Revisi Kolo  -->
+        <div class="mt-3 mx-auto">
+          <table>
+            <tr>
+              <td class="">Sering sakit pinggang?</td>
+              <td class="font-medium" id="sakit_pinggang">: No</td>
+            </tr>
+            <tr>
+              <td class="">Kencing di malam hari lebih sering?</td>
+              <td class="font-medium" id="nokturia">: No</td>
+            </tr>
+            <tr>
+              <td class="">Perlu mengejan saat awal kencing?</td>
+              <td class="font-medium" id="mengejan">: No</td>
+            </tr>
+            <tr>
+              <td class="">Urin lemah saat kencing?</td>
+              <td class="font-medium" id="urin_lemah">: No</td>
+            </tr>
+            <tr>
+              <td class="">Urin menetes setelah kencing?</td>
+              <td class="font-medium" id="urin_menetes">: No</td>
+            </tr>
+            <tr>
+              <td class="">Kencing terasa tidak tuntas?</td>
+              <td class="font-medium" id="kencing_tidak_tuntas">: No</td>
+            </tr>
+            <tr>
+              <td class="">Urin tidak keluar sama sekali?</td>
+              <td class="font-medium" id="urin_tidak_keluar_sama_sekali">
+                : No
+              </td>
+            </tr>
+            <tr>
+              <td class="">Terdapat darah ketika kencing?</td>
+              <td class="font-medium" id="darah_dalam_urin">: No</td>
+            </tr>
+            <tr>
+              <td class="">Nyeri saat kencing?</td>
+              <td class="font-medium" id="nyeri_saat_kencing">: No</td>
+            </tr>
+            <tr>
+              <td class="">Inkontensia urin/beser?</td>
+              <td class="font-medium" id="inkonsia_urin">: No</td>
+            </tr>
+          </table>
+          <hr class="mt-3" />
+        </div>
+        <!-- End Revisi Kolo  -->
+        <div class="mt-3 mx-auto">
+          <p class="mt-4 text-center font-medium">
+            Dari Keterangan Di Atas, Anda Terindikasi Gejala
+          </p>
+          <div
+            class="showHasil mt-4 rounded text-white w-1/2 mx-auto text-center"
+            :class="{
+              'bg-red-500': isRedBackground,
+              'bg-yellow-400': isYellowBackground,
+              'bg-green-500': isGreenBackground,
+            }"
+          >
             {{ textHasil }}
           </div>
         </div>
       </TheCard>
     </div>
     <div class="space-20"></div>
-    <Button class="mt-4 mx-auto" @click="backToDashboard">
+    <Button class="mt-4 mb-4 mx-auto" @click="backToDashboard">
       OK, back to Dashboard
     </Button>
   </div>
+  <div class="container" style="height: 70px"></div>
 </template>
 
 <style>
@@ -51,48 +109,97 @@
 </style>
 
 <script setup>
-import Navbar from "../layouts/Navbar.vue";
-import { onMounted, ref } from "vue";
-import { Input, Button, TheCard } from "flowbite-vue";
-import router from "../router";
+import Navbar from '../layouts/Navbar.vue';
+import { onMounted, ref } from 'vue';
+import { Input, Button, TheCard } from 'flowbite-vue';
+import router from '../router';
 
-const isRedBackground = ref(false)
-const isYellowBackground = ref(false)
-const isGreenBackground = ref(false)
-const textHasil = ref('')
+const isRedBackground = ref(false);
+const isYellowBackground = ref(false);
+const isGreenBackground = ref(false);
+const textHasil = ref('');
 
 onMounted(() => {
-  const nama = sessionStorage.getItem("nama");
-  const usia = sessionStorage.getItem("usia");
-  const alamat = sessionStorage.getItem("alamat");
-  const hasil = sessionStorage.getItem("hasil");
+  const nama = sessionStorage.getItem('nama');
+  const usia = sessionStorage.getItem('usia');
+  const alamat = sessionStorage.getItem('alamat');
+  const hasil = sessionStorage.getItem('hasil');
 
+  // Revisi Kolo
+
+  const umur = sessionStorage.getItem('umur');
+  const sakit_pinggang = sessionStorage.getItem('sakit_pinggang');
+  const mengejan = sessionStorage.getItem('mengejan');
+  const urin_lemah = sessionStorage.getItem('urin_lemah');
+  const urin_menetes = sessionStorage.getItem('urin_menetes');
+  const kencing_tidak_tuntas = sessionStorage.getItem('kencing_tidak_tuntas');
+  const nokturia = sessionStorage.getItem('nokturia');
+  const inkonsia_urin = sessionStorage.getItem('inkonsia_urin');
+  const nyeri_saat_kencing = sessionStorage.getItem('nyeri_saat_kencing');
+  const darah_dalam_urin = sessionStorage.getItem('darah_dalam_urin');
+  const urin_tidak_keluar_sama_sekali = sessionStorage.getItem(
+    'urin_tidak_keluar_sama_sekali'
+  );
 
   document.getElementById('nama_pasien').textContent = `: ${nama}`;
   document.getElementById('usia_pasien').textContent = `: ${usia}`;
   document.getElementById('alamat_pasien').textContent = `: ${alamat}`;
 
-  if (hasil == "Berat") {
-    isRedBackground.value = true
-    isYellowBackground.value = false
-    isGreenBackground.value = false
-    textHasil.value = hasil;
-  } else if (hasil == "Ringan") {
-    isRedBackground.value = false
-    isYellowBackground.value = true
-    isGreenBackground.value = false
-    textHasil.value = hasil;
-  } else if (hasil == "Tidak Terkena") {
-    isRedBackground.value = false
-    isYellowBackground.value = false
-    isGreenBackground.value = true
-    textHasil.value = hasil;
+  if (sakit_pinggang == 1) {
+    document.getElementById('sakit_pinggang').textContent = `: Yes`;
+  }
+  if (mengejan == 1) {
+    document.getElementById('mengejan').textContent = `: Yes`;
+  }
+  if (urin_lemah == 1) {
+    document.getElementById('urin_lemah').textContent = `: Yes`;
+  }
+  if (urin_menetes == 1) {
+    document.getElementById('urin_menetes').textContent = `: Yes`;
+  }
+  if (kencing_tidak_tuntas == 1) {
+    document.getElementById('kencing_tidak_tuntas').textContent = `: Yes`;
+  }
+  if (nokturia == 1) {
+    document.getElementById('nokturia').textContent = `: Yes`;
+  }
+  if (inkonsia_urin == 1) {
+    document.getElementById('inkonsia_urin').textContent = `: Yes`;
+  }
+  if (nyeri_saat_kencing == 1) {
+    document.getElementById('nyeri_saat_kencing').textContent = `: Yes`;
+  }
+  if (darah_dalam_urin == 1) {
+    document.getElementById('darah_dalam_urin').textContent = `: Yes`;
+  }
+  if (urin_tidak_keluar_sama_sekali == 1) {
+    document.getElementById(
+      'urin_tidak_keluar_sama_sekali'
+    ).textContent = `: Yes`;
   }
 
+  // End Revisi Kolo
+
+  if (hasil == 'Berat') {
+    isRedBackground.value = true;
+    isYellowBackground.value = false;
+    isGreenBackground.value = false;
+    textHasil.value = hasil;
+  } else if (hasil == 'Ringan') {
+    isRedBackground.value = false;
+    isYellowBackground.value = true;
+    isGreenBackground.value = false;
+    textHasil.value = hasil;
+  } else if (hasil == 'Tidak Terkena') {
+    isRedBackground.value = false;
+    isYellowBackground.value = false;
+    isGreenBackground.value = true;
+    textHasil.value = hasil;
+  }
 });
 
 // function
 function backToDashboard() {
-  router.push("/");
+  router.push('/');
 }
 </script>
